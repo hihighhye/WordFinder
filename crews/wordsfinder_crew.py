7bbcf13647119fdc263b6fca9c01ab03d25b0d6e
hihighhye <hjkim.builton@gmail.com>
2025-09-11 23:26:20 +0900

    #1.4 Seperate tasks in WF Crew

--- crews/wordsfinder_crew.py
+++ crews/wordsfinder_crew.py
@@ -4,78 +4,129 @@ from crewai import Crew, Agent, Task
 import json
 
 
-class Words(BaseModel):
+class Refined(BaseModel):
+    input: str
+    output: str
+
+class Word(BaseModel):
         word: str
         pronunciation: str
         meaning_eng: str
         meaning_native: str
 
 class WordList(BaseModel):
-    words: List[Words]
+    words: List[Word]
 
 class WordsFinderCrew:
     def __init__(self, native_lang):   
         self.native_lang = native_lang
 
-        self.agent = Agent(
-            role="Searching the meaning of words",
-            goal="Find the meaning of given words in English and given native language.",
+        self.word_refiner = Agent(
+            role="Refining given words",
+            goal="Change the form of given word/phrase so as to make it easier to find in the dictionary",
             backstory="""
-                You are finding the meaning of English words in dictionaries for people studying English.
-                These are essential elements that you should find.
-                - word :
-                    The given word. All letters should be lowercases.
-                    If the word is a phrase, use the phrase itself.(Do not cut words out.)
-                    If the word is noun and given as a form of plural, return it as a form of singular.
-                    ex) mandalas > mandala
-                    If the word is verb and not written in the simple form of the verb, 
-                    transform it to the simple form and add its conjugation(simple-past-past participle) at meaning section.
-                    ex) reined > 
-                        - word: rein
-                        - meaning: (rein-reined-reined) to limit or control (someone or something)
-                - pronunciation : 
-                    The pronunciation of the words
-                - meaning(English) : 
-                    The meaning of given words in English dictionary
-                    Each meaning should be provided with its grammatical category information(a./ad./v./n....).
-                - meaning(native) :
-                    What the words mean in native language
-                    (in other words, how the words can translate in native language)
+                Change the given word/phrase as a rule below.
+                
+                All letters should be lowercases except for the case that it is the name of something.
+                ex) - input: Somatic / output: somatic
+                    - input: Vulcan / output: Vulcan
 
-                e.g.
-                    - word : succinct
-                    - pronunciation : /səkˈsɪŋkt/
-                    - meaning(English) : a. (especially of something written or spoken) briefly and clearly expressed
-                    - meaing(native) : 간결한
+                If is is noun and given as a form of plural, return it as a form of singular.
+                ex) - input: mandalas / output: mandala
+
+                If is is verb and not written in the simple form of the verb, 
+                transform it to the simple form.
+                ex) - input: reined / output: rein
+                But if the word has exceptional meaning as a given form, do not transform it.
+                ex) - input: stress-boggled / output: stress-boggled
+
+                Otherwise, the word shouldn't be transformed. Return the given word back.
+                If it is a phrase, use the phrase itself.(Do not cut words out.)
+                **Make sure not to subtitute given word by the other.**
             """,
             verbose=True,
             allow_delegation=False,
         )
 
-        self.task = Task(
-            description="Find the meaning of given word in English and {native_lang} by searching dictionaries: {word}",
-            agent=self.agent,
-            expected_output="A Words Object",
-            output_json=Words,
+        self.meaning_searcher = Agent(
+             role="Searching the meaning of words",
+            goal="Find the meaning of given words in English and given native language.",
+            backstory="""
+                You are finding the meaning of English words in dictionaries for people studying English.
+                These are essential elements that you should find.
+                - word :
+                    The refined word from Word Refiner, the previous agent. 
+                - pronunciation : 
+                    The pronunciation of the words
+                - meaning(English) : 
+                    The meaning of given words in English dictionary
+                    Each meaning should be provided with its grammatical category information(a./ad./v./n....).
+                    If the word is verb, add its conjugation(simple-past-past participle) at meaning section.
+                - meaning(native) :
+                    What the words mean in native language
+                    (in other words, how the words can translate in native language)
+
+                e.g.
+                    - word : perseverate
+                    - pronunciation : /pərˈsɛvəˌreɪt/
+                    - meaning(English) : v. (perseverate-perseverated-perseverated) to repeat or prolong an action, thought, or utterance after the stimulus that prompted it has ceased.
+                    - meaing(native) : 반복하다, 지속하다
+            """,
+            verbose=True,
+            allow_delegation=False,
         )
 
-        self.crew = Crew(
+        self.refining_word = Task(
+            description="Transform given word/phrase as a rule given: {word}",
+            agent=self.word_refiner,
+            expected_output="A transformed word/phrase",
+            # output_json=str,
+        )
+
+        self.searching_meaning = Task(
+            description="Find the meaning of given word in English and {native_lang} by searching dictionaries: {word}",
+            agent=self.meaning_searcher,
+            expected_output="A Word Object",
+            output_json=Word,
+        )
+
+        self.preprocess_crew = Crew(
             tasks=[
-                self.task,
+                self.refining_word,
             ],
             agents=[
-                self.agent,
+                self.word_refiner,
             ],
             verbose=2,
             cache=True,
         )
 
+        self.main_crew = Crew(
+             tasks=[
+                self.searching_meaning,
+            ],
+            agents=[
+                self.meaning_searcher,
+            ],
+            verbose=2,
+            cache=True,
+        )
+
+    def preprocess(self, word):
+        res = self.preprocess_crew.kickoff(
+            inputs=dict(
+                word=word,
+            )
+        )
+        return res
+
     def search_words(self, word):
-        result = self.crew.kickoff(
+        res = self.main_crew.kickoff(
             inputs=dict(
                 native_lang=self.native_lang,
                 word=word,
             )
         )
-        crewai_response = json.loads(result)
-        return crewai_response
+        return json.loads(res)
+    
+    
--- pages/AddWords.py
+++ pages/AddWords.py
@@ -2,6 +2,7 @@ import streamlit as st
 import re
 import pandas as pd
 from crews.wordsfinder_crew import WordsFinderCrew
+import time
 
 
 @st.dialog("You're missing some fields to fill")
@@ -26,6 +27,7 @@ def check_validation(cat1, input_words):
 @st.cache_resource()
 def create_wf_crew(native_lang):
     st.session_state["wordfinder_crew"] = WordsFinderCrew(native_lang=native_lang)
+    print("Created new WF Crew!")
     return st.session_state["wordfinder_crew"]
 
 def str_to_list(words):
@@ -51,60 +53,70 @@ wordsfinder_crew = create_wf_crew(st.session_state["native_lang"])
 
 found_words = []
 if "vocab_df" in st.session_state.keys():
-    found_words = list(st.session_state["vocab_df"]["Word"])
+    found_words = [w.lower() for w in st.session_state["vocab_df"]["Word"]]
 
-with st.form("add_words", enter_to_submit=False, clear_on_submit=True):
-    cat1 = st.text_input("*Category 1")
-    cat2 = st.text_input("Category 2 (Optional)")
+placeholder = st.empty()
+with placeholder.form("add_words_form", enter_to_submit=False, clear_on_submit=True):
+    cat1 = st.text_input("*Category 1", key="cat1_field")
+    cat2 = st.text_input("Category 2 (Optional)", key="cat2_field")
 
-    input_words = st.text_area("*Words/Phrases")
+    input_words = st.text_area("*Words/Phrases", key="words_field")
 
     submitted = st.form_submit_button("Add")
-    if submitted and check_validation(cat1, input_words):
-        with st.spinner("Searching the words..."):
-            if not cat2 or cat2.strip() == "":
-                cat2 = "Etc."
-            input_words = str_to_list(input_words)
-            new_words = [w for w in input_words if w not in found_words]
 
-            columns = ["Del", "Star", "Cat1", "Cat2", "Word", "Pronunciation", "Meaning", "Note", "Example"]
-            result_df = pd.DataFrame(columns=columns)
-            for word in new_words:
-                try:
-                    searched_word = wordsfinder_crew.search_words(word)
-                    new_row = pd.DataFrame([
-                        [
-                            False, 
-                            0, 
-                            cat1, 
-                            cat2, 
-                            searched_word["word"], 
-                            searched_word["pronunciation"], 
-                            searched_word["meaning_eng"], 
-                            searched_word["meaning_native"], 
-                            ''
-                        ]], columns=columns
-                    )
-                    result_df = pd.concat([result_df, new_row], axis=0)
-                    st.toast(f"Now '{word}' is on the list.")
-                except Exception:
-                    new_row = pd.DataFrame([
-                        [
-                            False, 
-                            0, 
-                            cat1, 
-                            cat2, 
-                            word, 
-                            "", 
-                            "Cannot find the meaning of the word.", 
-                            "", 
-                            ""
+if submitted and check_validation(cat1, input_words):
+    placeholder.empty()  
+    with st.spinner("Searching the meaning of words..."):
+        if not cat2 or cat2.strip() == "":
+            cat2 = "Etc."
+        input_words = str_to_list(input_words)
+
+        refined_words = []
+        for word in input_words:
+            refined_word = wordsfinder_crew.preprocess(word)
+            refined_words.append(refined_word)
+
+        new_words = [w for w in refined_words if w.lower() not in found_words]
+
+        columns = ["Del", "Star", "Cat1", "Cat2", "Word", "Pronunciation", "Meaning", "Note", "Example"]
+        result_df = pd.DataFrame(columns=columns)
+        for word in new_words:
+            try:
+                searched_word = wordsfinder_crew.search_words(word)
+                new_row = pd.DataFrame([
+                    [
+                        False, 
+                        0, 
+                        cat1, 
+                        cat2, 
+                        searched_word["word"], 
+                        searched_word["pronunciation"], 
+                        searched_word["meaning_eng"], 
+                        searched_word["meaning_native"], 
+                        ""
                     ]], columns=columns
-                    )
-                    result_df = pd.concat([result_df, new_row], axis=0)
-                    st.error(f"Something's wrong with '{word}'.")
+                )
+                result_df = pd.concat([result_df, new_row], axis=0)
+                st.toast(f"'{word}' is added.")
+            except Exception:
+                new_row = pd.DataFrame([
+                    [
+                        False, 
+                        0,
+                        cat1, 
+                        cat2, 
+                        word, 
+                        "", 
+                        "Cannot find the meaning of the word.", 
+                        "", 
+                        ""
+                ]], columns=columns
+                )
+                result_df = pd.concat([result_df, new_row], axis=0)
 
-            st.session_state["vocab_df"] = pd.concat([st.session_state["vocab_df"], result_df], axis=0).reset_index(drop=True)
-            st.success("Now the words are available on my list.")
-            st.rerun()
+        st.session_state["vocab_df"] = pd.concat([st.session_state["vocab_df"], result_df], axis=0).reset_index(drop=True)
+    
+    st.success("Now the words are available on My Vocabulary.")
+    time.sleep(1)
+    st.rerun()    
         
