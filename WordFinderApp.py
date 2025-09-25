dbc222850218e3bf02ae5aa3842e18cbaba1b2f0
hihighhye <hjkim.builton@gmail.com>
2025-09-25 11:42:03 +0900

    #2.11 Minor correction - clear cache whenever resetting API key

--- WordFinderApp.py
+++ WordFinderApp.py
@@ -48,6 +48,7 @@ def set_up():
             except Exception as e:
                 st.error("Please enter valid OpenAI API key.")
                 st.session_state.pop("user_openai_api_key", None)
+                st.cache_resource.clear()
 
         else:
             st.error("Please enter valid OpenAI API key.")
